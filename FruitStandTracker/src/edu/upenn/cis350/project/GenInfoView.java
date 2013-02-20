73d9b994752676904e2a01b3f4f890d84340c738
mocamoca3 <monicalaskos@gmail.com>
2013-02-20 01:38:23 -0500

    School information, volunteer information, date information
    I attempted to make the necessary files and changed existing files.

---
+++ FruitStandTracker/src/edu/upenn/cis350/project/GenInfoView.java
@@ -0,0 +1,23 @@
+package edu.upenn.cis350.project;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GenInfoView extends View{
+	
+	public GenInfoView(Context c){
+		super(c);
+		init();
+	}
+	
+	public GenInfoView(Context c, AttributeSet a){
+		super(c, a);
+		init();
+	}
+	
+	protected void init(){
+		
+	}
+	
+}

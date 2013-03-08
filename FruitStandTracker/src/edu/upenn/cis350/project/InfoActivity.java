461e904713c0c978f1769faec6697c30c4993ebd
mocamoca3 <monicalaskos@gmail.com>
2013-03-08 14:13:55 -0500

    Changed the date input
    The current date should show up as a text field.
    Below it, there should be a button that allows the user to change the
    date. If we want to have an arbitrary date to begin with, we can change
    the onCreatedDialog method in the InfoActivity activity.

--- FruitStandTracker/src/edu/upenn/cis350/project/InfoActivity.java
+++ FruitStandTracker/src/edu/upenn/cis350/project/InfoActivity.java
@@ -1,6 +1,10 @@
 package edu.upenn.cis350.project;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,10 +14,21 @@ import android.view.ViewParent;
 import android.view.ViewStub;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class InfoActivity extends Activity {
+	
+	private TextView saleDisplayDate;
+	private Button changeDate;
+	
+	private int year;
+	private int month;
+	private int day;
+	
+	static final int DATE_DIALOG_ID = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,10 @@ public class InfoActivity extends Activity {
         
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        
+        //for the date picker
+        setCurrentDateOnView();
+		addListenerOnButton();
     }
 
     @Override
@@ -39,6 +58,74 @@ public class InfoActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_start, menu);
         return true;
     }
+     
+    
+ // display current date
+	public void setCurrentDateOnView() {
+ 
+		saleDisplayDate = (TextView) findViewById(R.id.saleDate);
+ 
+	    //auto populating the date
+		final Calendar c = Calendar.getInstance();
+		year = c.get(Calendar.YEAR);
+		month = c.get(Calendar.MONTH);
+		day = c.get(Calendar.DAY_OF_MONTH);
+ 
+		// set current date into textview
+		saleDisplayDate.setText(new StringBuilder()
+			// Month is 0 based, just add 1
+			.append(month + 1).append("-").append(day).append("-")
+			.append(year).append(" "));
+ 
+	}
+ 
+	public void addListenerOnButton() {
+		 
+		changeDate = (Button) findViewById(R.id.change_date);
+ 
+		changeDate.setOnClickListener(new View.OnClickListener() {
+ 
+			@Override
+			public void onClick(View v) {
+ 
+				showDialog(DATE_DIALOG_ID);
+ 
+			}
+ 
+		});
+ 
+	}
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DATE_DIALOG_ID:
+		   // set date picker as current date
+		   return new DatePickerDialog(this, datePickerListener, 
+                         year, month,day);
+		}
+		return null;
+	}
+	
+ 
+	private DatePickerDialog.OnDateSetListener datePickerListener 
+    		= new DatePickerDialog.OnDateSetListener() {
+
+			// when dialog box is closed, below method will be called.
+			public void onDateSet(DatePicker view, int selectedYear,
+				int selectedMonth, int selectedDay) {
+				year = selectedYear;
+				month = selectedMonth;
+				day = selectedDay;
+
+				// set selected date into textview
+				saleDisplayDate.setText(new StringBuilder().append(month + 1)
+						.append("-").append(day).append("-").append(year)
+						.append(" "));
+			}
+		};
+	
+	
     
     public void continueToWeather(View v) {
     	//Launch to weather

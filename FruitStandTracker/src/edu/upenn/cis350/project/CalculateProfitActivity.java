140f6f34ec284183dd4bc491e8df92a0088d9196
Boyang Niu <boyang.niu@gmail.com>
2013-03-27 09:51:01 -0400

    exception catching for blank profit calc boxes

--- FruitStandTracker/src/edu/upenn/cis350/project/CalculateProfitActivity.java
+++ FruitStandTracker/src/edu/upenn/cis350/project/CalculateProfitActivity.java
@@ -41,59 +41,64 @@ public class CalculateProfitActivity extends Activity {
 		DataBaser db = DataBaser.getInstance();
 
 		// Get all the doubles from the user input
-		double revenue = data.getDouble("total_cash");
-		double fruitCosts = getDoubleFromEditText(R.id.fruitStandCost);
-		double smoothieCosts = getDoubleFromEditText(R.id.smoothieCost);
-		double otherCosts = getDoubleFromEditText(R.id.otherCosts);
-		double totalComputed = getDoubleFromEditText(R.id.totalCosts);
-		double donations = getDoubleFromEditText(R.id.donationsAmount);
-		double profitComputed = getDoubleFromEditText(R.id.profit);
-		double endingCashbox = getDoubleFromEditText(R.id.endingCashbox);
-
-		String[] errorMsgs = {
-				"Looks like there were some errors with your input!\n", "", "",
-				"", "Try again!\n" };
-
-		// Only prevent them from moving on the first time.
-		boolean preventCompletion = !errors;
-		double totalReal = fruitCosts + smoothieCosts + otherCosts;
-		if (Math.abs(totalReal - totalComputed) > .05) {
-			errorMsgs[1] = "Total costs should be equal to fruit costs + smoothie costs + other costs.\n";
-			errors = true;
-		}
-		double profitReal = revenue + donations - totalReal;
-		if (Math.abs(profitReal - profitComputed) > .05) {
-			errorMsgs[2] = "Profit should equal revenue + donations - total costs\n";
-			errors = true;
-		}
-		double realEndingCash = revenue + donations
-				+ db.getStartingCashboxAmount();
-		if (Math.abs(endingCashbox - realEndingCash) > .50) {
-			errorMsgs[3] = "Ending cash should equal starting cash + revenue + donations\n"
-					+ "Are you sure you counted all of the cash?\n";
-			errors = true;
-		}
-		if(preventCompletion) {
-			StringBuffer toastText = new StringBuffer();
-			for(int i=0; i< errorMsgs.length;i++) {
-				toastText.append(errorMsgs[i]);
+		try {
+			double revenue = data.getDouble("total_cash");
+			double fruitCosts = getDoubleFromEditText(R.id.fruitStandCost);
+			double smoothieCosts = getDoubleFromEditText(R.id.smoothieCost);
+			double otherCosts = getDoubleFromEditText(R.id.otherCosts);
+			double totalComputed = getDoubleFromEditText(R.id.totalCosts);
+			double donations = getDoubleFromEditText(R.id.donationsAmount);
+			double profitComputed = getDoubleFromEditText(R.id.profit);
+			double endingCashbox = getDoubleFromEditText(R.id.endingCashbox);
+	
+			String[] errorMsgs = {
+					"Looks like there were some errors with your input!\n", "", "",
+					"", "Try again!\n" };
+	
+			// Only prevent them from moving on the first time.
+			boolean preventCompletion = !errors;
+			double totalReal = fruitCosts + smoothieCosts + otherCosts;
+			if (Math.abs(totalReal - totalComputed) > .05) {
+				errorMsgs[1] = "Total costs should be equal to fruit costs + smoothie costs + other costs.\n";
+				errors = true;
 			}
+			double profitReal = revenue + donations - totalReal;
+			if (Math.abs(profitReal - profitComputed) > .05) {
+				errorMsgs[2] = "Profit should equal revenue + donations - total costs\n";
+				errors = true;
+			}
+			double realEndingCash = revenue + donations
+					+ db.getStartingCashboxAmount();
+			if (Math.abs(endingCashbox - realEndingCash) > .50) {
+				errorMsgs[3] = "Ending cash should equal starting cash + revenue + donations\n"
+						+ "Are you sure you counted all of the cash?\n";
+				errors = true;
+			}
+			if(preventCompletion) {
+				StringBuffer toastText = new StringBuffer();
+				for(int i=0; i< errorMsgs.length;i++) {
+					toastText.append(errorMsgs[i]);
+				}
+				Toast.makeText(getApplicationContext(), toastText.toString(), Toast.LENGTH_LONG).show();
+				return;
+			}
+			db.addInfo("revenue", String.valueOf(revenue));
+			db.addInfo("total_costs", String.valueOf(totalReal));
+			db.addInfo("donations", String.valueOf(donations));
+			db.addInfo("profit", String.valueOf(profitReal));
+			db.addInfo("ending_cashbox_amount", String.valueOf(endingCashbox));
+			//saves the data to Parse
+			for(String s: data.keySet()){
+				db.addInfo(s, data.get(s).toString());
+			}
+			DataBaser.getInstance().databaseItThoroughly();
+			db.databaseItThoroughly();
+			
+			this.startActivity(new Intent(this, StartActivity.class));
+		} catch (Exception e) {
+			String toastText = "Looks like you left a box blank, everything has to be filled in before submitting.";
 			Toast.makeText(getApplicationContext(), toastText.toString(), Toast.LENGTH_LONG).show();
-			return;
 		}
-		db.addInfo("revenue", String.valueOf(revenue));
-		db.addInfo("total_costs", String.valueOf(totalReal));
-		db.addInfo("donations", String.valueOf(donations));
-		db.addInfo("profit", String.valueOf(profitReal));
-		db.addInfo("ending_cashbox_amount", String.valueOf(endingCashbox));
-		//saves the data to Parse
-		for(String s: data.keySet()){
-			db.addInfo(s, data.get(s).toString());
-		}
-		DataBaser.getInstance().databaseItThoroughly();
-		db.databaseItThoroughly();
-		
-		this.startActivity(new Intent(this, StartActivity.class));
 	}
 
 }

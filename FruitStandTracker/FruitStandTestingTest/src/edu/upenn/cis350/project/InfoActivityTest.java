7efd049d2e6c07e5202db56fc1b4457a5f0400f4
mocamoca3 <monicalaskos@gmail.com>
2013-03-26 23:26:06 -0400

    Changed variables from private to package and made it so that the app will not continue if a request has been made to add a school and the box has not been filled out
    Unfortunately, if you choose to add a school, do not fill out the
    resultant box, and then hit continue, the app does not proceed to the
    next screen. This is expected behavior; what is not expected is that
    the app will not go to the next screen even after a school has been
    entered. Need to change code that determines when the “Add a school”
    option has been chosen, not being done in the simplest manner at the
    moment. This may improve functionality.
    
    Also did a little more with the test cases

--- FruitStandTracker/FruitStandTestingTest/src/edu/upenn/cis350/project/InfoActivityTest.java
+++ FruitStandTracker/FruitStandTestingTest/src/edu/upenn/cis350/project/InfoActivityTest.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class InfoActivityTest extends ActivityInstrumentationTestCase2<SaleActivity>{
 	InfoActivity activity;
+	Spinner mySpinner;
 	
 	public InfoActivityTest() {
 		super("edu.upenn.cis350.project",InfoActivity.class);
@@ -15,6 +16,7 @@ public class InfoActivityTest extends ActivityInstrumentationTestCase2<SaleActiv
 	protected void setUp() throws Exception {
 		super.setUp();
 		act = getActivity();
+		mySpinner = (Spinner)act.findViewById(edu.upenn.cis350.project.R.id.school);
 		
 	}
 	
@@ -47,7 +49,19 @@ public class InfoActivityTest extends ActivityInstrumentationTestCase2<SaleActiv
 	
 	public void testSchoolAuden(){
 		act.school = null;
+		mySpinner.setSpinnerPosition(0);
+		act.getSchool();
 		String mySchool = "Auden Reid High School";
 		assertEquals(mySchool, act.school);
 	}
+	
+	public void testSchoolCommunications(){
+		act.school = null;
+		mySpinner.setSpinnerPosition(4);
+		act.getSchool();
+		String mySchool = "Communications Technology High School";
+		assertEquals(mySchool, act.school);
+	}
+	
+	
 }

05b14c306e95a5ae678b3246acefdb300e7baa0a
Daniel Wei <weyichen@seas.upenn.edu>
2013-03-27 00:15:21 -0400

    Added methods for +/- inventory buttons
    buttons crash program

--- FruitStandTracker/src/edu/upenn/cis350/project/InventoryActivity.java
+++ FruitStandTracker/src/edu/upenn/cis350/project/InventoryActivity.java
@@ -6,15 +6,13 @@ import com.parse.PushService;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.text.Editable;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.GridView;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,56 +25,115 @@ public class InventoryActivity extends Activity {
 	int kiwis = 0;
 	int grapes = 0;
 	int granola = 0;
-	int smoothie = 0;
+	int frozen = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inventory);
-		
-//	    GridView gridview = (GridView) findViewById(R.id.InventoryGrid);
-//	    gridview.setAdapter(new InventoryImageAdapter(this));
-//
-//	    gridview.setOnItemClickListener(new OnItemClickListener() {
-//	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-//	            switch(position) {
-//	            case 0: apples++;
-//	            		TextView apples_qty = (TextView) findViewById(R.id.apple_qty);
-//	            		apples_qty.setText(Integer.toString(apples));
-//	            		break;
-//	            case 1: pears++;
-//	            		TextView pears_qty = (TextView) findViewById(R.id.pear_qty);
-//	            		pears_qty.setText(Integer.toString(pears));
-//	            		break;
-//	            case 2: oranges++;
-//        				TextView oranges_qty = (TextView) findViewById(R.id.orange_qty);
-//        				oranges_qty.setText(Integer.toString(oranges));	            
-//	            		break;
-//	            case 3: grapes++;
-//						TextView grapes_qty = (TextView) findViewById(R.id.grapes_qty);
-//						grapes_qty.setText(Integer.toString(grapes));	
-//        				break;
-//	            case 4: kiwis++;
-//						TextView kiwis_qty = (TextView) findViewById(R.id.kiwi_qty);
-//						kiwis_qty.setText(Integer.toString(kiwis));	
-//						break;
-//	            case 5: bananas++;
-//						TextView bananas_qty = (TextView) findViewById(R.id.banana_qty);
-//						bananas_qty.setText(Integer.toString(bananas));	
-//						break;
-//	            case 6: granola++;
-//						TextView granolas_qty = (TextView) findViewById(R.id.granola_qty);
-//						granolas_qty.setText(Integer.toString(granola));	
-//	            		break;
-//	            case 7: smoothie++;
-//						TextView smoothies_qty = (TextView) findViewById(R.id.smoothie_qty);
-//						smoothies_qty.setText(Integer.toString(smoothie));	
-//	            		break;
-//	            }
-//	            
-//	            
-//	        }
-//	    });
+
+	}
+	
+	public void applePlus (View view) {
+		EditText appleQty = (EditText) findViewById(R.id.appleQty);
+		Editable qty_e = appleQty.getText();
+		if (qty_e != null) {
+			apples = Integer.parseInt(appleQty.getText().toString());
+		} else {
+			apples = 0;
+		}
+		apples++;
+		appleQty.setText(apples);
+	}
+	
+	public void appleMinus (View view) {
+		EditText appleQty = (EditText) findViewById(R.id.appleQty);
+		apples = Integer.parseInt(appleQty.getText().toString());
+		appleQty.setText(apples--);
+	}
+	
+	public void bananaPlus (View view) {
+		EditText bananaQty = (EditText) findViewById(R.id.bananaQty);
+		bananas = Integer.parseInt(bananaQty.getText().toString());
+		bananaQty.setText(bananas++);
+	}
+	
+	public void bananaMinus (View view) {
+		EditText bananaQty = (EditText) findViewById(R.id.bananaQty);
+		bananas = Integer.parseInt(bananaQty.getText().toString());
+		bananaQty.setText(bananas--);
+	}
+	
+	public void grapesPlus (View view) {
+		EditText grapesQty = (EditText) findViewById(R.id.grapesQty);
+		grapes = Integer.parseInt(grapesQty.getText().toString());
+		grapesQty.setText(grapes++);
+	}
+	
+	public void grapesMinus (View view) {
+		EditText grapesQty = (EditText) findViewById(R.id.grapesQty);
+		grapes = Integer.parseInt(grapesQty.getText().toString());
+		grapesQty.setText(grapes--);
+	}
+	
+	public void kiwiPlus (View view) {
+		EditText kiwiQty = (EditText) findViewById(R.id.kiwiQty);
+		kiwis = Integer.parseInt(kiwiQty.getText().toString());
+		kiwiQty.setText(kiwis++);
+	}
+	
+	public void kiwiMinus (View view) {
+		EditText kiwiQty = (EditText) findViewById(R.id.kiwiQty);
+		kiwis = Integer.parseInt(kiwiQty.getText().toString());
+		kiwiQty.setText(kiwis--);
+	}
+	
+	public void orangePlus (View view) {
+		EditText orangeQty = (EditText) findViewById(R.id.orangeQty);
+		oranges = Integer.parseInt(orangeQty.getText().toString());
+		orangeQty.setText(oranges++);
+	}
+	
+	public void orangeMinus (View view) {
+		EditText orangeQty = (EditText) findViewById(R.id.orangeQty);
+		oranges = Integer.parseInt(orangeQty.getText().toString());
+		orangeQty.setText(oranges--);
+	}
+	
+	public void pearPlus (View view) {
+		EditText pearQty = (EditText) findViewById(R.id.pearQty);
+		pears = Integer.parseInt(pearQty.getText().toString());
+		pearQty.setText(pears++);
+	}
+	
+	public void pearMinus (View view) {
+		EditText pearQty = (EditText) findViewById(R.id.pearQty);
+		pears = Integer.parseInt(pearQty.getText().toString());
+		pearQty.setText(pears--);
+	}
+	
+	public void granolaPlus (View view) {
+		EditText granolaQty = (EditText) findViewById(R.id.granolaQty);
+		granola = Integer.parseInt(granolaQty.getText().toString());
+		granolaQty.setText(granola++);
+	}
+	
+	public void granolaMinus (View view) {
+		EditText granolaQty = (EditText) findViewById(R.id.granolaQty);
+		granola = Integer.parseInt(granolaQty.getText().toString());
+		granolaQty.setText(granola--);
+	}
+	
+	public void frozenPlus (View view) {
+		EditText frozenQty = (EditText) findViewById(R.id.frozenQty);
+		frozen = Integer.parseInt(frozenQty.getText().toString());
+		frozenQty.setText(frozen++);
+	}
+	
+	public void frozenMinus (View view) {
+		EditText frozenQty = (EditText) findViewById(R.id.frozenQty);
+		frozen = Integer.parseInt(frozenQty.getText().toString());
+		frozenQty.setText(frozen--);
 	}
 
 	@Override
@@ -87,7 +144,7 @@ public class InventoryActivity extends Activity {
 	}
 	
 	public void continueToInventory2(View v) {
-    	//Launch to weather
+    	//Launch to inventory2
     	Intent i = new Intent(this, Inventory2Activity.class);
     	//Save our info
     	//TODO Use savePreInventory() to save info
